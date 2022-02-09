375e589e6c2c4866810dbfe0b834c4761d3b3334
Ross Keenan <rossk29@gmail.com>
2022-02-09 19:57:57 +0200

    fix: Link to edit pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,12 +25,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/SkepticMystic/breadcrumbs",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: "https://github.com/SkepticMystic/breadcrumbs",
+          editUrl: "https://github.com/SkepticMystic/BC-wiki",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
