284ac69372b526229e614d649f28da9ea4764c85
Ross Keenan <rossk29@gmail.com>
2022-02-09 19:43:13 +0200

    fix: Reference to old `intro` note

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
         items: [
           {
             type: "doc",
-            docId: "intro",
+            docId: "Home",
             position: "left",
             label: "Tutorial",
           },
