581edde295afaa5383c7ec046b30452d6e934b09
Ross Keenan <rossk29@gmail.com>
2022-02-12 15:28:59 +0200

    fix: Only warn onBrokenLinks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: "Flexible hierarchies in Obsidian.md",
   url: "https://breadcrumbs-wiki.onrender.com",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
   organizationName: "SkepticMystic", // Usually your GitHub org/user name.
